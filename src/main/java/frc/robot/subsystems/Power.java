2fe41294c57511c248dc518492307613a02a44f9
DylanWhite2246Programming <whited200550@gmail.com>
2022-04-11 20:52:22 -0400

    added methods

--- src/main/java/frc/robot/subsystems/Power.java
+++ src/main/java/frc/robot/subsystems/Power.java
@@ -4,11 +4,36 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Ports;
 
 public class Power extends SubsystemBase {
+  
+  ShuffleboardTab power = Shuffleboard.getTab("Power");
+  NetworkTable powerTable = NetworkTableInstance.getDefault().getTable("Power");
+
+  PowerDistribution PDP = new PowerDistribution(Ports.kPDPCANID, ModuleType.kRev);
   /** Creates a new Power. */
-  public Power() {}
+  public Power() {
+    PDP.setSwitchableChannel(false);
+
+    
+  }
+
+  public void setLights(boolean on){PDP.setSwitchableChannel(on);}
+
+  public double getAmpDraw(){return PDP.getTotalCurrent();}
+  public double getPower(){return PDP.getTotalPower();}
+  public double getAmpDraw(int channel){return PDP.getCurrent(channel);}
+  
+  
 
   @Override
   public void periodic() {
