0b029635a3e17a90b07a273c542d11c0b1b1a68e
DylanWhite2246Programming <whited200550@gmail.com>
2022-04-11 20:53:23 -0400

    unused import

--- src/main/java/frc/robot/subsystems/Power.java
+++ src/main/java/frc/robot/subsystems/Power.java
@@ -5,7 +5,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
