7d17ec05a8fd02b731ba38f7363844355bb3fdfc
DylanWhite2246Programming <whited200550@gmail.com>
2022-04-11 21:14:07 -0400

    added logging

--- src/main/java/frc/robot/subsystems/Power.java
+++ src/main/java/frc/robot/subsystems/Power.java
@@ -4,12 +4,16 @@
 
 package frc.robot.subsystems;
 
+import java.util.Map;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.WidgetType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Ports;
 
@@ -23,16 +27,23 @@ public class Power extends SubsystemBase {
   public Power() {
     PDP.setSwitchableChannel(false);
 
-    
+    power.addNumber("Voltage", this::getVoltage)
+      .withWidget(BuiltInWidgets.kDial)
+      .withProperties(Map.of("min",0,"max",13));
+    power.addNumber("Amp Draw", this::getAmpDraw)
+      .withWidget(BuiltInWidgets.kDial)
+      .withProperties(Map.of("min",0,"max",120));
+    power.addNumber("Power", this::getPower);
+    power.add(PDP)
+      .withWidget(BuiltInWidgets.kPowerDistribution);
   }
 
   public void setLights(boolean on){PDP.setSwitchableChannel(on);}
 
+  public double getVoltage(){return PDP.getVoltage();}
   public double getAmpDraw(){return PDP.getTotalCurrent();}
   public double getPower(){return PDP.getTotalPower();}
-  public double getAmpDraw(int channel){return PDP.getCurrent(channel);}
-  
-  
+  public double getAmpDraw(int channel){return PDP.getCurrent(channel);}  
 
   @Override
   public void periodic() {
